0ab84cf06456d855200bf8ba9c63e1f3a13aa2c1
JasonZOH <jasonzoh2@gmail.com>
2024-09-05 18:15:52 +0200

    modificationde fichier test

--- data/testData.js
+++ data/testData.js
@@ -5,7 +5,7 @@ const COURSES = [
     id: '1',
     title: 'Fomation JavaScript Moderne',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2020/08/22/23/29/content-marketing-5509584_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_1280.png',
     price: 99.99,
     selected: false,
     instructorId: '1'
@@ -14,7 +14,7 @@ const COURSES = [
     id: '2',
     title: 'Coder facebook avec React JS',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2020/11/23/22/06/facebook-5771058_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2017/06/22/06/22/facebook-2429746_1280.png',
     price: 49.99,
     selected: false,
     instructorId: '1'
@@ -23,7 +23,7 @@ const COURSES = [
     id: '3',
     title: 'Wordpress pour débutants',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2021/02/15/20/38/software-6019023_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2016/03/29/11/48/wordpress-1288020_1280.png',
     price: 29.49,
     selected: false,
     instructorId: '1'
@@ -32,7 +32,7 @@ const COURSES = [
     id: '4',
     title: 'Maîtiser le logiciel Confluence',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2020/04/29/13/05/graphics-5108828_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2020/05/13/15/44/geometry-5167943_1280.jpg',
     price: 49.29,
     selected: false,
     instructorId: '1'
@@ -50,7 +50,7 @@ const COURSES = [
     id: '6',
     title: 'apprendre CSS3 ',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2021/03/20/21/31/presentation-6110845_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2016/11/19/23/00/css3-1841590_1280.png',
     price: 9.99,
     selected: false,
     instructorId: '1'
@@ -59,7 +59,7 @@ const COURSES = [
     id: '7',
     title: 'Formation PHP Orienté Objets',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2020/11/08/05/07/computer-5722873_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2015/09/17/17/31/code-944504_1280.jpg',
     price: 29.99,
     selected: false,
     instructorId: '1'
@@ -68,7 +68,7 @@ const COURSES = [
     id: '8',
     title: 'Formation SEO de 0 à PRO!',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2020/09/18/17/35/computer-5582499_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2016/02/19/12/37/seo-1210158_1280.jpg',
     price: 19.49,
     selected: false,
     instructorId: '1'
@@ -77,7 +77,7 @@ const COURSES = [
     id: '9',
     title: 'Formation Microsoft Clarity',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2021/02/15/20/44/software-6019044_960_720.png',
+    image: 'https://cdn.pixabay.com/photo/2019/11/07/07/48/microsoft-4608125_1280.jpg',
     price: 29.45,
     selected: false,
     instructorId: '1'
