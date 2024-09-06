227c61da351623affe661f508cf962d825b8fd1b
JasonZOH <jasonzoh2@gmail.com>
2024-09-06 08:15:39 +0200

    modification des images de testData

--- data/testData.js
+++ data/testData.js
@@ -1,11 +1,11 @@
 // Images Pixabay: https://pixabay.com/fr/users/jmexclusives-10518280/
 
-const COURSES = [
+export const COURSES = [
   {
     id: '1',
     title: 'Fomation JavaScript Moderne',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_1280.png',
+    image: 'https://img.freepik.com/vecteurs-libre/programmeurs-utilisant-langage-programmation-javascript-ordinateur-personnes-minuscules-langage-javascript-moteur-javascript-concept-developpement-web-js_335657-2412.jpg?ga=GA1.1.367324415.1721312328&semt=ais_hybrid',
     price: 99.99,
     selected: false,
     instructorId: '1'
@@ -14,7 +14,7 @@ const COURSES = [
     id: '2',
     title: 'Coder facebook avec React JS',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2017/06/22/06/22/facebook-2429746_1280.png',
+    image: 'https://img.freepik.com/vecteurs-libre/reagissez-illustration-concept-abstrait-application-mobile-native-framework-developpement-applications-mobiles-natives-multiplateformes-bibliotheque-javascript-interface-utilisateur-systeme-exploitation_335657-3350.jpg?ga=GA1.1.367324415.1721312328&semt=ais_hybrid',
     price: 49.99,
     selected: false,
     instructorId: '1'
@@ -23,7 +23,7 @@ const COURSES = [
     id: '3',
     title: 'Wordpress pour débutants',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2016/03/29/11/48/wordpress-1288020_1280.png',
+    image: 'https://img.freepik.com/vecteurs-libre/illustration-page-accueil-design-plat-dessine-main_23-2149255390.jpg?ga=GA1.1.367324415.1721312328&semt=ais_hybrid',
     price: 29.49,
     selected: false,
     instructorId: '1'
@@ -32,7 +32,7 @@ const COURSES = [
     id: '4',
     title: 'Maîtiser le logiciel Confluence',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2020/05/13/15/44/geometry-5167943_1280.jpg',
+    image: 'https://img.freepik.com/vecteurs-libre/personne-travaillant-pour-developpement-applications_23-2148703944.jpg?ga=GA1.1.367324415.1721312328',
     price: 49.29,
     selected: false,
     instructorId: '1'
@@ -41,7 +41,7 @@ const COURSES = [
     id: '5',
     title: 'Python pour les débutants',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2021/04/24/22/12/python-coding-6205177_960_720.png',
+    image: 'https://img.freepik.com/vecteurs-libre/illustration-coloree-du-programmeur-travaillant_23-2148281410.jpg?ga=GA1.1.367324415.1721312328&semt=ais_hybrid',
     price: 199.99,
     selected: false,
     instructorId: '1'
@@ -50,7 +50,7 @@ const COURSES = [
     id: '6',
     title: 'apprendre CSS3 ',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2016/11/19/23/00/css3-1841590_1280.png',
+    image: 'https://img.freepik.com/vecteurs-libre/langages-programmation-css-html-programmation-informatique-codage-informatique-personnage-dessin-anime-programmeur-feminin-logiciel-developpement-sites-web_335657-760.jpg?ga=GA1.1.367324415.1721312328&semt=ais_hybrid',
     price: 9.99,
     selected: false,
     instructorId: '1'
@@ -59,7 +59,7 @@ const COURSES = [
     id: '7',
     title: 'Formation PHP Orienté Objets',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2015/09/17/17/31/code-944504_1280.jpg',
+    image: 'https://img.freepik.com/vecteurs-libre/concept-developpement-applications-langages-programmation_23-2148688949.jpg?ga=GA1.1.367324415.1721312328',
     price: 29.99,
     selected: false,
     instructorId: '1'
@@ -68,7 +68,7 @@ const COURSES = [
     id: '8',
     title: 'Formation SEO de 0 à PRO!',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2016/02/19/12/37/seo-1210158_1280.jpg',
+    image: 'https://img.freepik.com/vecteurs-premium/developpement-du-site-web_773186-1177.jpg?ga=GA1.1.367324415.1721312328',
     price: 19.49,
     selected: false,
     instructorId: '1'
@@ -77,7 +77,7 @@ const COURSES = [
     id: '9',
     title: 'Formation Microsoft Clarity',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!',
-    image: 'https://cdn.pixabay.com/photo/2019/11/07/07/48/microsoft-4608125_1280.jpg',
+    image: 'https://img.freepik.com/vecteurs-libre/illustration-degrade-sql_23-2149251015.jpg?ga=GA1.1.367324415.1721312328&semt=ais_hybrid',
     price: 29.45,
     selected: false,
     instructorId: '1'
