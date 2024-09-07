014e3a29da9e3a1415eaa966b5fccae255d7ace9
JasonZOH <jasonzoh2@gmail.com>
2024-09-07 13:11:25 +0200

    Creation du composant CourseInfos

---
+++ screens/CourseInfos.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CourseInfos = () => {
+  return (
+    <View>
+      <Text>CourseInfos</Text>
+    </View>
+  )
+}
+
+export default CourseInfos
+
+const styles = StyleSheet.create({})
