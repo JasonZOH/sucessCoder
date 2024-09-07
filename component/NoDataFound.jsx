8e201b3cd6ca43ea894c879b92557cd0b8c3a315
JasonZOH <jasonzoh2@gmail.com>
2024-09-07 13:08:45 +0200

    Creation d'un composant pour les données non trouvées

---
+++ component/NoDataFound.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import globalStyles from '../styles/globalStyles'
+
+const NoDataFound = ({ text }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  )
+}
+
+export default NoDataFound
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    alignItems: 'center',
+  },
+  text: {
+    color: globalStyles.green,
+    fontSize: 19
+  }
+})
