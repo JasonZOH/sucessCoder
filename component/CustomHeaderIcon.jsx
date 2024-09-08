9fe814f538a2c8c369064226832b138de0ec6545
JasonZOH <jasonzoh2@gmail.com>
2024-09-08 11:48:42 +0200

    Composant pour un icon dans le Header

---
+++ component/CustomHeaderIcon.jsx
@@ -0,0 +1,21 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import globalStyles from "../styles/globalStyles";
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+
+const CustomHeaderIcon = ({ nav }) => {
+  return (
+    <TouchableOpacity onPress={() => nav.navigate('Cart')}>
+      <View style={styles.styleButtonHeader}>
+        <MaterialIcons name="shopping-cart" size={24} color={globalStyles.white} />
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  styleButtonHeader: {
+    marginHorizontal: 15
+  }
+});
+
+export default CustomHeaderIcon
